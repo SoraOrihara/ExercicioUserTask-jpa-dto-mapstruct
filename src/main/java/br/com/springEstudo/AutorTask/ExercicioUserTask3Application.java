caa2e513c1046060f6c584fa81506d31de253a05
SoraOrihara <caca66z@gmail.com>
2025-09-06 11:10:26 -0300

    Criação do projeto e dos packages que serão utilizados

---
+++ src/main/java/br/com/springEstudo/AutorTask/ExercicioUserTask3Application.java
@@ -0,0 +1,13 @@
+package br.com.springEstudo.AutorTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioUserTask3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioUserTask3Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springEstudo/AutorTask/ExercicioUserTask3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springEstudo.AutorTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioUserTask3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
