8592cd6c73b7ec963de6c8d7f4e81fb913467c05
SoraOrihara <caca66z@gmail.com>
2025-09-16 12:33:59 -0300

    hotfixes.01

--- src/test/java/br/com/springEstudo/AutorTask/infraestructure/repositories/UserRepositoryTest.java
+++ src/test/java/br/com/springEstudo/AutorTask/infraestructure/repositories/UserRepositoryTest.java
@@ -9,8 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 import br.com.springEstudo.AutorTask.business.dto.UserRequestDto;
